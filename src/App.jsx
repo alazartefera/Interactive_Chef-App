b1bdf7aa7b9bbeca399a0e98c0edcc981132c75f
Alazar <alazartefera028@gmail.com>
2025-04-05 10:27:48 +0300

    fix build errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
-import { useState } from 'react'
-import Header from './components/Header'
-import Main from './components/Main'
-import recipeCode from './components/recipeCode'
-import './index.css'
+import Header from "./components/Header";
+import Main from "./components/Main";
+import "./index.css";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-     <Header/>
-     <Main/>
+      <Header />
+      <Main />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
